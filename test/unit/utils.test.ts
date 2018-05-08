3dfae77634645909929a8a6ef8cce2b87b8e4e17
Wade Tandy <wtandy@bloomberg.net>
2018-05-08 01:10:10 -0400

    Fix gitignore

---
+++ test/unit/utils.test.ts
@@ -0,0 +1,95 @@
+import { expect } from '../test-helper'
+import { spy, assert as sinonAssert } from 'sinon'
+import { minifyAst, recursiveMinify } from '../../src/utils'
+import { MinifedASTElement, MinifiedASTNode, MinifiedASTText } from 'vue-svg-component-runtime'
+import { mount } from 'vue-test-utils'
+
+describe('recursiveMinify', () => {
+  context('when the node is a text node', () => {
+    it('returns just the text field', () => {
+      let node : any = {
+        text: 'foobar',
+        other: 'value'
+      }
+
+      expect(recursiveMinify(node)).to.deep.equal({text: 'foobar'})
+    })
+
+    context('when the node has empty text', () => {
+      it('returns undefined', () => {
+        let node : any = {
+          text: '   ',
+          other: 'value'
+        }
+        expect(recursiveMinify(node)).to.be.undefined
+      })
+    })
+  })
+
+  context('when the node is an element node', () => {
+    it('returns only necessary nodes and recursively parses children', () => {
+      let node : any = {
+        tag: 'svg',
+        attrsMap: {
+          first: 'firstVal',
+          second: 'secondVal',
+        },
+        children: [{
+          text: 'foobar',
+          other: 'value'
+        },{
+          text: ''
+        }],
+        other: 'value'
+      }
+
+      expect(recursiveMinify(node)).to.deep.equal({
+        tag: 'svg',
+        attrsMap: {
+          first: 'firstVal',
+          second: 'secondVal',
+        },
+        children: [{
+          text: 'foobar',
+        }],
+      })
+    })
+
+    context('when the children are empty', () => {
+      it('returns node without children', () => {
+        let node : any = {
+          tag: 'svg',
+          attrsMap: {
+            attr: 'attrval',
+          },
+          children: []
+        }
+        expect(recursiveMinify(node)).to.deep.equal({
+          tag: 'svg',
+          attrsMap: {
+            attr: 'attrval',
+          },
+        })
+      })
+    })
+
+    context('when the attrsMap hash is blank', () => {
+      it('returns node without attrsMap', () => {
+        let node : any = {
+          tag: 'svg',
+          attrsMap: {
+          },
+          children: [{
+            text: 'foobar',
+          }],
+        }
+        expect(recursiveMinify(node)).to.deep.equal({
+          tag: 'svg',
+          children: [{
+            text: 'foobar',
+          }],
+        })
+      })
+    })
+  })
+})
